f61337bf4b066695b1113e26abce15531bea9328
yorigum <yorigum@gmail.com>
2022-10-23 10:42:15 +0700

    fix gradle and plugins

--- YoMovies/data/src/main/java/com/yoriworks/data/repository/dataSourceImpl/MoveRemoteDataSourceImpl.kt
+++ YoMovies/data/src/main/java/com/yoriworks/data/repository/dataSourceImpl/MoveRemoteDataSourceImpl.kt
@@ -1,12 +1,9 @@
 package com.yoriworks.data.repository.dataSourceImpl
 
+import com.yoriworks.data.BuildConfig
 import com.yoriworks.data.api.MovieApi
 import com.yoriworks.data.repository.dataSource.MovieRemoteDataSource
-import com.yoriworks.domain.model.MovieList
-import com.yoriworks.yomovies.BuildConfig
-import retrofit2.Response
 
 class MoveRemoteDataSourceImpl(private val movieApi: MovieApi):MovieRemoteDataSource {
     override suspend fun getPopularMovies() = movieApi.getPopularMovies(BuildConfig.API_KEY)
-
 }
